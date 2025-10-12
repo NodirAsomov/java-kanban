3cd7c02804fb337be02760eb9705642f1ba6fc9f
NodirAsomov <147218602+NodirAsomov@users.noreply.github.com>
2025-10-12 22:29:58 +0500

    Update Epic.java

--- src/taskapp/Epic.java
+++ src/taskapp/Epic.java
@@ -10,6 +10,7 @@ public  class Epic extends Task {
     public Epic(String name, String description) {
         super(name, description, Status.NEW);
     }
+    
     public Epic(int id, String name, String description) {
         super(id, name, description, Status.NEW);
     }
